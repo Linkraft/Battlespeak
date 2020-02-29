853369c5342be8e03a9eb636fab547d22d8e7893
Linkraft <36479127+Linkraft@users.noreply.github.com>
2020-02-29 12:29:33 -0500

    Initial commit for coding the logic of the game

--- battleship.py
+++ battleship.py
@@ -1,20 +1,41 @@
 import speech_recognition as sr
-r = sr.Recognizer()
 
-mic = sr.Microphone(device_index=1)
+def printRules():
+    print("     Welcome to Battlespeak!\n")
+    print("This is a voice-controlled version of the popular")
+    print("board game Battleship. To play, you must give commands")
+    print("to the program with your voice.\n")
+    printCommands()
 
-print(sr.Microphone.list_microphone_names())
+def printCommands():
+    print("Here are the list of commands available to you:")
+    print("- To tell the program what coordinates you would like")
+    print("  to bomb, please say the coordinates in this form:")
+    print("                 \"Bomb D1\"")
+    print("  where \"D1\" would be replaced with your desired coordinates")
 
-while True:
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
 
-    try:
-        print(r.recognize_google(audio))
-        break
-    except sr.RequestError:
-        print("The Google API didn't work for some reason")
-        print("Make sure this computer is connected to the Internet")
-    except sr.UnknownValueError:
-        print("Woops! You just spoke some nonsense. Try again!")
+if __name__ == "__main__":
+    r = sr.Recognizer()
+
+    mic = sr.Microphone(device_index=1)
+
+    print(sr.Microphone.list_microphone_names())
+
+    printRules()
+
+    while True:
+        with mic as source:
+            print("Hold on a moment...")
+            r.adjust_for_ambient_noise(source)
+            print("Now say your command:")
+            audio = r.listen(source)
+
+        try:
+            print(r.recognize_google(audio))
+            break
+        except sr.RequestError:
+            print("The Google API didn't work for some reason")
+            print("Make sure this computer is connected to the Internet")
+        except sr.UnknownValueError:
+            print("Woops! You just spoke some nonsense. Try again!")
